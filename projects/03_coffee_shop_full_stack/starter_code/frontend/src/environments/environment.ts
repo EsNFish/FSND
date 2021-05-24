10410d4610b17cbf945c3ec3d37a94a3b189b107
Brandon Fisher <esnfish@gmail.com>
2021-05-24 17:29:22 -0500

    chore: add auth tokens to postman and clean up a file

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,14 +1,10 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
-  production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
-  auth0: {
-    url: 'fish-coffee-shop.us', // the auth0 domain prefix
-    audience: 'fish-coffee-shop', // the audience set for the auth0 app
-    clientId: 'tCKAf6nQrzAm8zCXRllogSAv4UpS2m5p', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
-  }
+    production: false,
+    apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+    auth0: {
+        url: 'fish-coffee-shop.us', // the auth0 domain prefix
+        audience: 'fish-coffee-shop', // the audience set for the auth0 app
+        clientId: 'tCKAf6nQrzAm8zCXRllogSAv4UpS2m5p', // the client id generated for the auth0 app
+        callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
+    }
 };
